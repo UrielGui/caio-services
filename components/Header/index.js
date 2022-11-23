7cbcda8eedcfc6d1818fb3de2e4a6e6f5a111157
Uriel <urieligara@hotmail.com>
2022-11-23 03:13:36 -0300

    logo updated

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,16 +4,13 @@ import { useTranslation } from 'react-i18next';
 export default function Header() {
   const { t } = useTranslation();
   return (
-    <header className='w-full bg-black2 py-6'>
+    <header className='w-full bg-defaultBlack2 py-6 shadow-[0_-3px_39px_19px_rgba(0,0,0,0.28)]'>
       <div className='default-container min-w-full text-white'>
         <div className='flex text-xl'>
+          <div className='place-self-center'>
+            <Image src='/img/logo.png' alt={t('head.title')} width={200} height={100} />
+          </div>
           <div className='flex flex-grow'>
-            <Image
-              src='/img/logo.png'
-              alt='Caio Services - IPTV Sem Quedas e Sem Travamentos'
-              width={224}
-              height={40}
-            />
             <nav className='flex text-white w-full mt-1 pl-4'>
               <ul className='flex px-2 cursor-pointer flex-grow'>
                 <li className='px-2'>{t('header.nav.option1')}</li>
