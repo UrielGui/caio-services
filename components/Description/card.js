b8ba8a933bdef3fc99e10cfa08e7dbdbc94d5b65
Uriel <urieligara@hotmail.com>
2022-11-24 19:06:56 -0300

    card created

---
+++ components/Description/card.js
@@ -0,0 +1,7 @@
+export default function Card({ background }) {
+  return (
+    <>
+      <div className={`${background} bg-no-repeat bg-cover bg-bottom w-44 h-60 bg-pink-800 rounded-2xl`} />
+    </>
+  );
+}
