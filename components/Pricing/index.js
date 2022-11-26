98f1e4c3565b8285de2f17d216d084a01055622a
Uriel <urieligara@hotmail.com>
2022-11-26 05:52:54 -0300

    Pricing created

--- components/Pricing/index.js
+++ components/Pricing/index.js
@@ -1,11 +1,39 @@
-import Image from 'next/image';
+import { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import PriceList from './priceList';
 
 export default function Pricing() {
+  const inputRefChecked = useRef(null);
+  const [inputChecked, setInputChecked] = useState(null);
   const { t } = useTranslation();
   return (
-    <section className='bg-orange-600'>
-      <h1>Pricing</h1>
+    <section className='bg-defaultBlack2 py-8 text-xl'>
+      <div className='default-container'>
+        <div className='text-center flex flex-col items-center py-6'>
+          <h1 className='text-3xl'>{t('pricing.title')}</h1>
+          <p className='mt-3'>{t('pricing.description')}</p>
+          <p className='w-4/5'>{t('pricing.description-2')}</p>
+          <div className='flex mt-10'>
+            <span>{t('pricing.option-1')}</span>
+            <div class='relative inline-block w-28 mr-2 align-middle select-none mx-4 mr-4 -top-2'>
+              <input
+                type='checkbox'
+                name='toggle'
+                id='toggle'
+                ref={inputRefChecked}
+                onChange={() => setInputChecked(inputRefChecked.current?.checked)}
+                className='toggle-checkbox absolute block w-10 h-10 rounded-full bg-white border-4 appearance-none cursor-pointer'
+              />
+              <label
+                htmlFor='toggle'
+                className='toggle-label block overflow-hidden h-10 rounded-full bg-defaultCyan cursor-pointer'
+              ></label>
+            </div>
+            <span>{t('pricing.option-2')}</span>
+          </div>
+          <PriceList inputChecked={inputChecked} />
+        </div>
+      </div>
     </section>
   );
 }
