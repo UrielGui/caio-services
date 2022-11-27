bdaec9b99f6e270877853420c8320ad4ece8c460
Uriel <urieligara@hotmail.com>
2022-11-27 06:04:18 -0300

    accordion created

--- components/Faq/index.js
+++ components/Faq/index.js
@@ -1,10 +1,85 @@
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+import { Accordion, AccordionHeader, AccordionBody } from '@material-tailwind/react';
 
 export default function Faq() {
   const { t } = useTranslation();
+  const [open, setOpen] = useState(1);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+
+  const customAnimation = {
+    mount: { scale: 1 },
+    unmount: { scale: 0.9 },
+  };
+
+  function Icon({ id, open }) {
+    return (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        className={`${id === open ? ' rotate-270' : 'rotate-30'} absolute top-2.5  h-5 w-5 transition-transform`}
+        fill='none'
+        viewBox='0 0 24 24'
+        stroke='currentColor'
+        strokeWidth={2}
+      >
+        <path d='M14 0L0 0L7 11L14 0Z' fill='white' />
+      </svg>
+    );
+  }
+
   return (
-    <section className='bg-yellow-600'>
-      <h1>FAQ</h1>
+    <section className='py-10 text-xl'>
+      <div className='default-container'>
+        <div className='text-center flex flex-col items-center py-6'>
+          <h1 className='text-3xl'>{t('faq.title')}</h1>
+        </div>
+        <div className='px-48 mt-10'>
+          <Accordion open={open === 1} animate={customAnimation} icon={''}>
+            <AccordionHeader onClick={() => handleOpen(1)}>
+              {<Icon id={1} open={open} />}
+              <h2 className='ml-8'>{t('faq.questions.question-1')}</h2>
+            </AccordionHeader>
+            <AccordionBody className='p-8 !opacity-50'>{t('faq.answers.answer-1')}</AccordionBody>
+            <hr className='border-slate-400 border-solid border-1 my-5' />
+          </Accordion>
+          <Accordion open={open === 2} animate={customAnimation} icon={''}>
+            <AccordionHeader onClick={() => handleOpen(2)}>
+              {<Icon id={2} open={open} />}
+              <h2 className='ml-8'>{t('faq.questions.question-2')}</h2>
+            </AccordionHeader>
+            <AccordionBody className='p-8 mt-4 !opacity-50'>{t('faq.answers.answer-2')}</AccordionBody>
+            <hr className='border-slate-400 border-solid border-1 my-5' />
+          </Accordion>
+
+          <Accordion open={open === 3} animate={customAnimation} icon={''}>
+            <AccordionHeader onClick={() => handleOpen(3)}>
+              {<Icon id={3} open={open} />}
+              <h2 className='ml-8'>{t('faq.questions.question-3')}</h2>
+            </AccordionHeader>
+            <AccordionBody className='p-8 mt-4 !opacity-50'>{t('faq.answers.answer-3')}</AccordionBody>
+            <hr className='border-slate-400 border-solid border-1 my-5' />
+          </Accordion>
+          <Accordion open={open === 4} animate={customAnimation} icon={''}>
+            <AccordionHeader onClick={() => handleOpen(4)}>
+              {<Icon id={4} open={open} />}
+              <h2 className='ml-8'>{t('faq.questions.question-4')}</h2>
+            </AccordionHeader>
+            <AccordionBody className='p-8 mt-4 !opacity-50'>{t('faq.answers.answer-4')}</AccordionBody>
+            <hr className='border-slate-400 border-solid border-1 my-5' />
+          </Accordion>
+          <Accordion open={open === 5} animate={customAnimation} icon={''}>
+            <AccordionHeader onClick={() => handleOpen(5)}>
+              {<Icon id={5} open={open} />}
+              <h2 className='ml-8'>{t('faq.questions.question-5')}</h2>
+            </AccordionHeader>
+            <AccordionBody className='p-8 mt-4 !opacity-50'>{t('faq.answers.answer-5')}</AccordionBody>
+            <hr className='border-slate-400 border-solid border-1 my-5' />
+          </Accordion>
+        </div>
+      </div>
     </section>
   );
 }
