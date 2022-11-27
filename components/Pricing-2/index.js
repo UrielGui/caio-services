5b93865908f8784e68a09f0033f100a056973ba1
Uriel <urieligara@hotmail.com>
2022-11-26 21:13:44 -0300

    Pricing2 created

--- components/Pricing-2/index.js
+++ components/Pricing-2/index.js
@@ -1,11 +1,60 @@
-import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 export default function Pricing2() {
   const { t } = useTranslation();
+  const defaultCheckedIcon = { color: '#0C7CE3', marginRight: '5px', fontSize: '1.2em' };
   return (
-    <section className='bg-pink-500'>
-      <h1>Pricing 2</h1>
+    <section className='py-14 text-xl bg-defaultBlack2'>
+      <div className='default-container'>
+        <div className='text-center flex flex-col items-center py-6'>
+          <h1 className='text-3xl'>{t('pricing-2.title')}</h1>
+          <p className='mt-3'>
+            {t('pricing-2.description')} <Link href='/'>{t('pricing-2.link-text')}</Link>
+          </p>
+        </div>
+        <div className='default-container min-w-full mt-8'>
+          <div className='flex text-xl'>
+            <div className='flex flex-grow flex-col '>
+              <h2>{t('pricing-2.description-2')}</h2>
+              <ul className='space-y-2 mt-6'>
+                <li className='flex align-items: center;'>
+                  <AiOutlineCheckCircle style={defaultCheckedIcon} />{' '}
+                  <span className='relative -top-0.5 text-xl'>{t('pricing-2.features.feature-1')}</span>
+                </li>
+                <li className='flex align-items: center;'>
+                  <AiOutlineCheckCircle style={defaultCheckedIcon} />{' '}
+                  <span className='relative -top-0.5 text-xl'>{t('pricing-2.features.feature-2')}</span>
+                </li>
+                <li className='flex align-items: center;'>
+                  <AiOutlineCheckCircle style={defaultCheckedIcon} />{' '}
+                  <span className='relative -top-0.5 text-xl'>{t('pricing-2.features.feature-3')}</span>
+                </li>
+                <li className='flex'>
+                  <AiOutlineCheckCircle style={defaultCheckedIcon} />{' '}
+                  <span className='relative -top-0.5 text-xl'>{t('pricing-2.features.feature-4')}</span>
+                </li>
+                <li className='flex'>
+                  <AiOutlineCheckCircle style={defaultCheckedIcon} />{' '}
+                  <span className='relative -top-0.5 text-xl'>{t('pricing-2.features.feature-5')}</span>
+                </li>
+                <li className='flex'>
+                  <AiOutlineCheckCircle style={defaultCheckedIcon} />{' '}
+                  <span className='relative -top-0.5 text-xl'>{t('pricing-2.features.feature-6')}</span>
+                </li>
+              </ul>
+              <button type='button' className='bg-defaultBlue w-64 mt-6 rounded-md py-3 px-5 mb-6'>
+                {t('pricing.plans.button')}
+              </button>
+            </div>
+            <div className='flex'>
+              <Image src='/img/management-panel.png' alt='iptv management panel' width={500} height={500} />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
