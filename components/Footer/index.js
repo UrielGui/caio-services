5a49ce3964c4220e7ed2c1f2a07f96f418349e0e
Uriel <urieligara@hotmail.com>
2022-11-27 06:50:21 -0300

    footer created

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,8 +4,20 @@ import { useTranslation } from 'react-i18next';
 export default function Footer() {
   const { t } = useTranslation();
   return (
-    <footer className='bg-green-600'>
-      <h1>Footer</h1>
+    <footer className='bg-defaultBlack2'>
+      <div className='default-container'>
+        <div className='flex text-xl py-4'>
+          <div className='flex flex-grow place-items-center'>
+            <span className='px-2'>
+              © 2022 <span className='font-bold'>{t('footer.copyright.name')}</span>.{' '}
+              {t('footer.copyright.all-rights-reserved')}
+            </span>
+          </div>
+          <div className='flex'>
+            <Image src='/img/payments-method.png' alt='payments method' width={300} height={100} />
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
