7a9259fad74fae30a052645fae88085351643397
Uriel <urieligara@hotmail.com>
2022-11-30 07:10:38 -0300

    swiper added

--- components/Description/index.js
+++ components/Description/index.js
@@ -1,30 +1,75 @@
 import { useTranslation } from 'react-i18next';
 import Card from './card';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/css';
 
 export default function Description() {
   const { t } = useTranslation();
+  SwiperCore.use([Autoplay]);
   return (
     <section id='description' className='py-8 text-xl'>
       <div className='text-center flex flex-col items-center py-6'>
         <h1 className='text-3xl'>{t('description.title')}</h1>
         <p className='w-2/4 mt-3'>{t('description.description')}</p>
       </div>
-      <div className='flex justify-center overflow-hidden relative'>
-        <div className='w-full flex w-max items-center space-x-16 animate-slide'>
-          <Card background="bg-[url('/img/carroussel/card-1.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-2.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-3.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-4.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-5.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-6.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-7.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-8.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-9.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-10.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-11.png')]" />
-          <Card background="bg-[url('/img/carroussel/card-12.png')]" />
+      <Swiper
+        slidesPerView={7}
+        spaceBetween={1}
+        slidesPerGroup={1}
+        loop={true}
+        speed={5000}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+        }}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+      >
+        <div className='flex justify-center overflow-hidden relative'>
+          <div className='w-full flex w-max items-center space-x-16'>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-1.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-2.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-3.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-4.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-5.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-6.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-7.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-8.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-9.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-10.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-11.png')]" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Card background="bg-[url('/img/carroussel/card-12.png')]" />
+            </SwiperSlide>
+          </div>
         </div>
-      </div>
+      </Swiper>
     </section>
   );
 }
