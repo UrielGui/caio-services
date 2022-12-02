75cf9322c0a5bbea3b5cdd78accedf8e02cf8f0d
Uriel <urieligara@hotmail.com>
2022-12-01 21:54:53 -0300

    code updated

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -4,18 +4,23 @@ import Link from 'next/link';
 export default function HeroSection() {
   const { t } = useTranslation();
   return (
-    <section id='hero-section' className='bg-hero-section-background bg-no-repeat bg-center bg-cover py-20'>
-      <div className='py-14 pr-20 pl-12 text-3xl w-2/5 text-center'>
-        <h1 className='text-4xl'>{t('hero-section.title')}</h1>
-        <h3 className='mt-2 text-3xl'>{t('hero-section.subtitle')}</h3>
-        <div className='mt-12'>
-          <Link href='https://wa.me/message/3KMEBBKUNJ4PF1' target='_blank'>
-            <button type='button' className='bg-defaultBlue rounded-md py-4 px-14'>
-              {t('hero-section.button')}
-            </button>
-          </Link>
-          <br />
-          <small className='block mt-3 text-xs px-8'>{t('hero-section.warning')}</small>
+    <section
+      id='hero-section'
+      className='lg:bg-hero-section-background bg-hero-section-mobile-background bg-no-repeat bg-center bg-cover py-20'
+    >
+      <div className='default-container'>
+        <div className='py-6 sm:py-14 lg:pr-20 text-3xl lg:w-2/5 text-center'>
+          <h1 className='2xl:text-4xl'>{t('hero-section.title')}</h1>
+          <h3 className='mt-2 2xl:text-3xl'>{t('hero-section.subtitle')}</h3>
+          <div className='mt-12'>
+            <Link href='https://wa.me/message/3KMEBBKUNJ4PF1' target='_blank'>
+              <button type='button' className='bg-defaultBlue rounded-md py-4 px-10 xl:px-10 lg:px-2.5'>
+                {t('hero-section.button')}
+              </button>
+            </Link>
+            <br />
+            <small className='block mt-3 text-xs px-8'>{t('hero-section.warning')}</small>
+          </div>
         </div>
       </div>
     </section>
--- components/HowItWorks/index.js
+++ components/HowItWorks/index.js
@@ -6,14 +6,14 @@ export default function HowItWorks() {
   return (
     <section id='how-it-works' className='py-12'>
       <div className='default-container'>
-        <div className='w-11/12'>
+        <div className='w-full md:w-11/12 flex items-center md:items-start flex-col'>
           <h1 className='text-3xl'>{t('how-it-works.title')}</h1>
           <p className='mt-6'>{t('how-it-works.description-1')}</p>
           <p className='mt-6'>{t('how-it-works.description-2')}</p>
           <p className='mt-6'>{t('how-it-works.description-3')}</p>
         </div>
-        <div className='w-full flex justify-center mt-4 space-x-32'>
-          <div className='w-3/12 flex flex-col items-center'>
+        <div className='w-full flex justify-center flex-col md:flex-row mt-8 md:mt-4 md:space-x-28 space-y-12 md:space-y-0 place-items-center'>
+          <div className='w-ful md:w-3/12 flex flex-col items-center'>
             <Image
               src='/img/step-by-step/step-1.png'
               alt={t('how-it-works.step-by-step-img-alt-1')}
@@ -22,7 +22,7 @@ export default function HowItWorks() {
             />
             <p className='mt-2'>{t('how-it-works.step-by-step-1')}</p>
           </div>
-          <div className='w-3/12 flex flex-col items-center'>
+          <div className='w-full md:w-3/12 flex flex-col items-center'>
             <Image
               src='/img/step-by-step/step-2.png'
               alt={t('how-it-works.step-by-step-img-alt-2')}
@@ -31,7 +31,7 @@ export default function HowItWorks() {
             />
             <p className='mt-2'>{t('how-it-works.step-by-step-2')}</p>
           </div>
-          <div className='w-3/12 flex flex-col items-center'>
+          <div className='w-ful md:w-3/12 flex flex-col items-center'>
             <Image
               src='/img/step-by-step/step-3.png'
               alt={t('how-it-works.step-by-step-img-alt-3')}
