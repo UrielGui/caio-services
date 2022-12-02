ebb60a0005c6cbfc6fb7fc3740f6b28a6858dd03
Uriel <urieligara@hotmail.com>
2022-12-01 22:19:34 -0300

    eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['prettier', 'prettier/react', 'plugin:react/recommended', 'airbnb'],
+  extends: ['prettier', 'eslint:recommended', 'next'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true }],
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
